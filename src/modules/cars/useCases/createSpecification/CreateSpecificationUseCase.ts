8b0a116b6278763afb01685366cdb2c14bd55e93
Wallace Camarinha <wallace.camarinha@gmail.com>
2021-09-17 02:19:41 -0300

    refactor: Specifications useCases

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,11 +1,11 @@
-import { ISpecificationsRepository } from '../repositories/ISpecificationRepository';
+import { ISpecificationsRepository } from '../../repositories/ISpecificationRepository';
 
 interface IRequest {
   name: string;
   description: string;
 }
 
-class CreateSpecificationService {
+class CreateSpecificationUseCase {
   constructor(private specificationsRepository: ISpecificationsRepository) {}
 
   execute({ name, description }: IRequest): void {
@@ -20,4 +20,4 @@ class CreateSpecificationService {
   }
 }
 
-export { CreateSpecificationService };
+export { CreateSpecificationUseCase };
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
 import express from 'express';
-
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationsRoutes);
+app.use(router);
 
 // eslint-disable-next-line no-console
 app.listen(3333, () => console.log('Server running on port 3333!'));
